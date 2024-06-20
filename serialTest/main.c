e36a258076bfe8c55bc6a6beb880270e7433ef1a
Maxwell Epstein <maxwell.epstein@columbia.edu>
2024-06-20 11:07:29 -0400

    added switch case in scan_for_input()

--- serialTest/main.c
+++ serialTest/main.c
@@ -5,7 +5,33 @@
 char num;
 char numCompare = 255;
 
-uint8_t block[10];
+// State Transition Stuff
+int state;
+#define AWAIT_HEADER 1
+#define AWAIT_SYNC 2
+
+
+void scan_for_input(){
+    char in;
+    while ( true ) {
+        scanf("%c", &in);
+
+        switch (state) {
+            case AWAIT_HEADER:
+                if (in == 0x55) {
+                    state = AWAIT_SYNC;
+                }
+                break;
+            
+            case AWAIT_SYNC:
+                if (in == 0x3C) {
+                    get_block();
+                }
+            default:
+                break;
+        }
+    }
+}
 
 uint16_t get_block(){
     uint16_t buffer_index;
